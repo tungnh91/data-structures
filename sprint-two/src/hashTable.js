d9fab5783bf5695de7cb0ace0339b59be143aed1
tungnh91 <tungnh91@gmail.com>
2017-02-10 14:12:42 -0800

    Pass tests 'should store values that were inserted' and 'should not contain values that were not inserted' and ' should overwrite values that have the same key'

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -6,11 +6,15 @@ var HashTable = function() {
 };
 
 HashTable.prototype.insert = function(k, v) {
+  console.log(this._storage);
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage[index] = [k, v];
+  console.log(this._storage);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage[index][1];
 };
 
 HashTable.prototype.remove = function(k) {
