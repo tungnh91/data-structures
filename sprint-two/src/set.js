3689c928e4a941453969c9c3010cef955302e027
tungnh91 <tungnh91@gmail.com>
2017-02-10 11:38:14 -0800

    Pass test 'should add values to a set'

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,15 +1,20 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = []; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage.push(item);
 };
 
 setPrototype.contains = function(item) {
+  if (this._storage.includes(item)) {
+    return true;
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
