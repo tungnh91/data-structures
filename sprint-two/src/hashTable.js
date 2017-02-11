cbe835ae973a6daf235b092e9ea54929ab68ff2a
tungnh91 <tungnh91@gmail.com>
2017-02-10 18:32:24 -0800

    Pass all tests for everything

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -23,17 +23,18 @@ HashTable.prototype.insert = function(k, v) {
     this._numberOfEntries++;
   }
   if (this._numberOfEntries / this._limit > .75) {
-    console.log(this._numberOfEntries);
     this.doubleSize();
   }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  for (var i = 0; i < this._storage[index].length; i++) {
-    if (this._storage[index][i] !== undefined) {
-      if (this._storage[index][i][0] === k) {
-        return this._storage[index][i][1];         
+  if (this._storage[index] !== undefined) {
+    for (var i = 0; i < this._storage[index].length; i++) {
+      if (this._storage[index][i] !== undefined) {
+        if (this._storage[index][i][0] === k) {
+          return this._storage[index][i][1];         
+        }
       }
     }
   }
@@ -44,12 +45,31 @@ HashTable.prototype.remove = function(k) {
   for (var i = 0; i < this._storage[index].length; i++) {
     if (this._storage[index][i][0] === k) {
       this._storage[index][i] = undefined;
+      this._numberOfEntries--;
     }
+  }
+  if (this._numberOfEntries / this._limit < .25) {
+    this.halveSize();
   }
 };
 
 HashTable.prototype.doubleSize = function() {
   var newTable = new HashTable(this._limit * 2);
+  for (var i = 0; i < this._limit; i++) {
+    if (this._storage[i] !== undefined) {
+      for (j = 0; j < this._storage[i].length; j++) {
+        if (this._storage[i][j] !== undefined) {
+          newTable.insert(this._storage[i][j][0], this._storage[i][j][1]);
+        }
+      }
+    }
+  }
+  this._limit = newTable._limit;
+  this._storage = newTable._storage;
+};
+
+HashTable.prototype.halveSize = function() {
+  var newTable = new HashTable(this._limit / 2);
   for (var i = 0; i < this._limit; i++) {
     if (this._storage[i] !== undefined) {
       for (j = 0; j < this._storage[i].length; j++) {
